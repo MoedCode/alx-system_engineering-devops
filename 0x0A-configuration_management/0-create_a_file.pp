42c76c18c7aa15b59c60f2bfdc22c8f378c06183
MoedCode <mohamed71291@gmail.com>
2023-12-23 03:46:46 +0200

    0. Create a file (automated command)

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# 0-create_a_file.pp
- file { '/tmp/school':
-   ensure  => present,
-   mode    => '0744'
-   owner   => 'www-data',
-   group   => 'www-data',
-   content => 'I love Puppet',
- }
+# create a file in /tmp.
+file { '/tmp/school':
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744'
+}
