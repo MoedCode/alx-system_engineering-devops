9d80af7bbdb599f809db0b131a7e338a5d7e40ab
MoedCode <mohamed71291@gmail.com>
2024-01-01 22:55:05 +0200

    2. Client configuration file (automated command)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# make  configuration file 100-puppet_ssh_config.pp
+
+# Turn off password authentication
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+# Declare identity file
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
+}
