be13a44c3e271bf7e906ac5d59288a913a97a905
MoedCode <mohamed71291@gmail.com>
2024-01-30 04:49:31 +0200

    1. Export to CSV (automated command)

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import csv
+    ID = argv[1]
+    URL = "https://jsonplaceholder.typicode.com/"
+    Employee = requests.get(URL + f"users/{ID}").json()
+    NAME = Employee.get("username")
+    TODO = requests.get(URL + "todos", params={"userId": ID}).json()
+
+    with open("{}.csv".format(ID), "w", newline="") as FILE:
+        writer = csv.writer(FILE, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [ID, NAME, t.get("completed"), t.get("title")]
+        ) for t in TODO]
