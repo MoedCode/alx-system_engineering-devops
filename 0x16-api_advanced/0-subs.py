905d68215fc54ec65cd4e5dceb143fbc0b3ef1ab
MoedCode <mohamed71291@gmail.com>
2024-02-14 00:53:10 +0200

    1. Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,18 @@ def number_of_subscribers(subreddit):
     """ tasks reddit user name and return the subscribers number """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-    Hdrs =  {'User-Agent': 'Mozilla/5.0'}
+    Hdrs = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=Hdrs,  allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        data = response.json()['data']['children']
+        if len(data):
+            for _ in data:
+                print(_['data']['title'])
+        else:
+            print("None")
+            return
+
     else:
+        print("None")
         return 0
-
-
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ tasks reddit user name and return the subscribers number """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    Hdrs = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=Hdrs,  allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
