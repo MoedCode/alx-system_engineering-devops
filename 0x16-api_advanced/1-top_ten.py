48e92c31a1cd54ed8936fd7bfbcf59227e7534c6
MoedCode <mohamed71291@gmail.com>
2024-02-14 03:22:50 +0200

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-import requests
+"""this is function file"""
 
 
 def top_ten(subreddit):
-    """Tasks Reddit user name and returns the subscribers number."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    """
+    prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    import requests
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    Hdrs = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=Hdrs, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        if data:
-            for post in data:
-                print(post['data']['title'])
-        else:
-            print("No posts found for the given subreddit.")
-    else:
+    if response.status_code != 200:
         print("None")
+        return
 
+    data = response.json()['data']['children']
 
-if __name__ == '__main__':
-    top_ten(sys.argv[1] if len(sys.argv) > 1 else '')
+    if len(data) == 0:
+        print("None")
+        return
+
+    for _ in data:
+        print(_['data']['title'])
